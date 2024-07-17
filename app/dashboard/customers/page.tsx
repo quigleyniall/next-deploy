58a9c95554d1f3a1697c7c6f107562e1e5b75351
Niall Quigley <niallquigley92@gmail.com>
2024-07-17 14:49:34 +1000

    Remove imports

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,6 @@
-import Pagination from "@/app/ui/invoices/pagination";
-import Search from "@/app/ui/search";
+
 import Table from "@/app/ui/customers/table";
-import { CreateInvoice } from "@/app/ui/invoices/buttons";
-import { lusitana } from "@/app/ui/fonts";
-import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
-import { Suspense } from "react";
-import { fetchFilteredCustomers, fetchInvoicesPages } from "@/app/lib/data";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
